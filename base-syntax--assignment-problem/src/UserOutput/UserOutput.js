0738d4fcf02a8d363a91a9b6115d02f1aaac5e5d
helloanh <ahoang10@jhu.edu>
2019-09-10 12:21:07 -0400

    create userInput and userOutput component

---
+++ base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const userOutput = () => {
+	return(
+		<div className="UserOutput">
+			<p> This is paragraph one. </p>
+			<p> This is paragraph two. </p>
+		</div>
+
+	);
+};
+
+
+export default userOutput;
