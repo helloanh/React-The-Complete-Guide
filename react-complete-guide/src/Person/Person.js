1141e7583ba31e8dfd04326e9acf6dc267f21d50
helloanh <ahoang10@jhu.edu>
2019-09-10 11:41:01 -0400

    adding Person.css component and import to Person.js

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
 	return(
 		// understanding children property
-		<div>
+		<div className="Person">
 			<p> I'm a {props.name} and I am {props.age} years old. </p>
 			<p>{props.children}</p>
 		</div>
